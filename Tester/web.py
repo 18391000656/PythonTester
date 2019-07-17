dede2f9a9df2ae84767fa116cb1cb322265a2f10
18391000656 <ZLbluesea0656@163.com>
2019-07-17 16:11:31 +0800

    完成文件上传开发
    Signed-off-by: 18391000656 <ZLbluesea0656@163.com>

--- Tester/web.py
+++ Tester/web.py
@@ -10,6 +10,14 @@ class Web:
         #打开浏览器
         self.driver = webdriver.Chrome()
 
+    #上传文件
+    def upload(self):
+        self.driver.implicitly_wait(20)
+        self.driver.get('https://www.baidu.com')
+        self.driver.find_element_by_xpath('//*[@id="form"]/span[1]/span').click()
+        self.driver.find_element_by_xpath('//*[@id="form"]/div/div[2]/div[2]/input').send_keys('D:\\PycharmProjects\\firstproject\\小猫.jpg')
+        time.sleep(10)
+
     def close(self):
         time.sleep(3)
         self.driver.quit()
--- Tester/webrunner.py
+++ Tester/webrunner.py
@@ -7,4 +7,8 @@ from Tester.web import Web #导入Web这个类
 
 #打开浏览器
 web = Web()
+
+#上传文件
+web.upload()
+
 web.close()
